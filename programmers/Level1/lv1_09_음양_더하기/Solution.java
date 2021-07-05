d31221847acc849a58d9767a1c7fd2108b01cef5
Haseop Lee <hsle1214@gmail.com>
2021-07-05 19:53:43 +0900

    Update 프로그래머스 문제 풀이(음양 더하기)

---
+++ programmers/Level1/lv1_09_음양_더하기/Solution.java
@@ -0,0 +1,27 @@
+package programmers.Level1.lv1_09_음양_더하기;
+
+class Solution {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    int[] absolutes = { 1, 2, 3 };
+    boolean[] signs = { true, false, true };
+
+    int result = solution.solution(absolutes, signs);
+    System.out.println("결과값 : " + result);
+  }
+
+  private int solution(int[] absolutes, boolean[] signs) {
+    int answer = 0;
+    int count = absolutes.length;
+    for (int i = 0; i < count; i++) {
+      if (signs[i]) {
+        answer += absolutes[i];
+      } else {
+        answer -= absolutes[i];
+      }
+    }
+    return answer;
+  }
+}
