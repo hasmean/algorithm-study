6623df642812b3a49dcf34dd59ad5548fab98023
Haseop Lee <hsle1214@gmail.com>
2021-07-07 00:20:25 +0900

    Fixed Java 코드는 4스페이스로 변경

--- programmers/Level1/lv1_01_체육복/Solution.java
+++ programmers/Level1/lv1_01_체육복/Solution.java
@@ -1,53 +1,53 @@
 package programmers.Level1.lv1_01_체육복;
 
 public class Solution {
-  public static void main(String[] args) {
-    Solution solution = new Solution();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
-    int totalStudent = 5;
-    int[] lostStudents = { 4, 5 };
-    int[] reserveStudents = { 1, 4 };
+        int totalStudent = 5;
+        int[] lostStudents = { 4, 5 };
+        int[] reserveStudents = { 1, 4 };
 
-    int result = solution.solution(totalStudent, lostStudents, reserveStudents);
-    System.out.println("결과: " + result + "명");
-  }
-
-  public int solution(int n, int[] lost, int[] reserve) {
-    int[] student = new int[n];
-
-    // 여벌의 체육복을 가지고 있는 학생 카운트 추가
-    for (int studentNumber : reserve) {
-      student[studentNumber - 1]++;
+        int result = solution.solution(totalStudent, lostStudents, reserveStudents);
+        System.out.println("결과: " + result + "명");
     }
 
-    // 체육복을 잃은 학생 카운트 제거
-    for (int studentNumber : lost) {
-      student[studentNumber - 1]--;
-    }
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] student = new int[n];
 
-    // 체육복 배분
-    for (int i = 0; i < student.length; i++) {
-      if (student[i] < 0) {
-        if (i != student.length - 1 && student[i + 1] > 0) {
-          student[i]++;
-          student[i + 1]--;
-        } else if (i != 0 && student[i - 1] > 0) {
-          student[i]++;
-          student[i - 1]--;
+        // 여벌의 체육복을 가지고 있는 학생 카운트 추가
+        for (int studentNumber : reserve) {
+            student[studentNumber - 1]++;
         }
-      }
+
+        // 체육복을 잃은 학생 카운트 제거
+        for (int studentNumber : lost) {
+            student[studentNumber - 1]--;
+        }
+
+        // 체육복 배분
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] < 0) {
+                if (i != student.length - 1 && student[i + 1] > 0) {
+                    student[i]++;
+                    student[i + 1]--;
+                } else if (i != 0 && student[i - 1] > 0) {
+                    student[i]++;
+                    student[i - 1]--;
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < student.length; i++) {
+            // 체육복을 가지고 있는 경우 학생 수 추가
+            if (0 <= student[i]) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
-
-    int answer = 0;
-
-    for (int i = 0; i < student.length; i++) {
-      // 체육복을 가지고 있는 경우 학생 수 추가
-      if (0 <= student[i]) {
-        answer++;
-      }
-    }
-
-    return answer;
-  }
 
 }
--- programmers/Level1/lv1_07_내적/Solution.java
+++ programmers/Level1/lv1_07_내적/Solution.java
@@ -1,23 +1,23 @@
 package programmers.Level1.lv1_07_내적;
 
 public class Solution {
-  public static void main(String[] args) {
-    Solution solution = new Solution();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
-    int[] a = { 1, 2, 3, 4 };
-    int[] b = { -3, -1, 0, 2 };
+        int[] a = { 1, 2, 3, 4 };
+        int[] b = { -3, -1, 0, 2 };
 
-    int result = solution.solution(a, b);
+        int result = solution.solution(a, b);
 
-    System.out.println("예상 결과 값 : 3 == " + result);
-  }
-
-  public int solution(int[] a, int[] b) {
-    int answer = 0;
-    int size = a.length;
-    for (int i = 0; i < size; i++) {
-      answer += a[i] * b[i];
+        System.out.println("예상 결과 값 : 3 == " + result);
     }
-    return answer;
-  }
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int size = a.length;
+        for (int i = 0; i < size; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
 }
